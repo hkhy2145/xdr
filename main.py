b68b6d17dd7383ad67ba737ea43c7baf75572dff
hkhy2145 <132103457+hkhy2145@users.noreply.github.com>
2023-10-28 23:11:41 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,41 @@
-"""
-Simple Hello World app to test Buildozer Action.
-
-It builds Kivy app with file main.kv.
-"""
-
+import kivy
 from kivy.app import App
-from kivy.lang import Builder
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+import requests  # You'll need to install this library if not already done
 
 
-class MainApp(App):
+class DiscordWebhookApp(App):
     def build(self):
-        return Builder.load_file("main.kv")
+        layout = BoxLayout(orientation='vertical', spacing=10, padding=10)
 
+        self.text_input = TextInput(hint_text="Enter your message")
+        send_button = Button(text="Send to Discord")
+        send_button.bind(on_press=self.send_to_discord)
 
-if __name__ == "__main__":
-    MainApp().run()
+        layout.add_widget(self.text_input)
+        layout.add_widget(send_button)
+
+        self.output_label = TextInput(hint_text="Response from Discord", readonly=True, multiline=True)
+
+        layout.add_widget(self.output_label)
+        return layout
+
+    def send_to_discord(self, instance):
+        message = self.text_input.text
+        webhook_url = "https://discord.com/api/webhooks/1165290854416646225/NFI2Puw2SYeWNetzEm9sr_KtCSjEA-6CS54hTQZDCy7LD-EYLuv0rM2oioO7ObazFZvU"
+        data = {
+            "content": message
+        }
+
+        response = requests.post(webhook_url, json=data)
+
+        if response.status_code == 204:
+            self.output_label.text = "Message sent to Discord successfully"
+        else:
+            self.output_label.text = f"Failed to send message to Discord: {response.status_code}"
+
+if __name__ == '__main__':
+    DiscordWebhookApp().run()
+
